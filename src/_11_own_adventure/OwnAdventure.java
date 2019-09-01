305b7390d6401b83a7e38d01b5fe64938aacfdc5
League Student <league@iMac-24.attlocal.net>
2019-09-01 11:26:19 -0700

    my adventure last week
    :alien:

---
+++ src/_11_own_adventure/OwnAdventure.java
@@ -0,0 +1,72 @@
+package _11_own_adventure;
+
+import javax.swing.JOptionPane;
+
+public class OwnAdventure {
+
+	
+	public static void main(String[] args) {
+		
+		JOptionPane.showInputDialog("Hello explorer, I'm Bob the builder, what is your name?");
+		String cat=
+	JOptionPane.showInputDialog("you come across a fork ion your path. Which direction would you like to go?"
+			+ " 'L' for  left, 'R' for right.");
+		
+		if(cat.equals("L")) {
+			String boi=JOptionPane.showInputDialog("You have encountered a world of barbie dolls."
+		+ "'H' for hide, 'C' for saying hi.");
+			if(boi.equals("C")) {
+				JOptionPane.showMessageDialog(null, "the barbie killed you");
+			}
+			else {
+				
+					String haha=	JOptionPane.showInputDialog("you escaped the barbie world but while running you met the"
+						+ " globglogabgalab 'P' for poking hole in him 'B'  "
+						+ "for giving him a book");
+
+	
+			
+			if(haha.equals("P")) {
+				JOptionPane.showMessageDialog(null, "MUHAHAHHAHAHA YOU CAN NEVER BEAT THE GLOB");
+			}
+			else {
+		String doggo=		JOptionPane.showInputDialog("The glob was very pleased with you and gave you a key."
+						+ " what will you do with the key? 'R' throw it away, 'K' keep it");
+		if(doggo.equals)
+			}
+			
+		}}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "Your dog got too hungry"
+					+ " so he had to eat you");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
